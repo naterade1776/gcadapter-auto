24cf68a990cc5d28cb88408eb7ec174206e4ddc1
Nate K <97563728+naterade1776@users.noreply.github.com>
2023-11-17 18:42:49 -0700

    Add files via upload

---
+++ gamecube-adapter.sh
@@ -0,0 +1,5 @@
+echo "Creating rules.d file....\n"
+touch /etc/udev/rules.d/51-gcadapter.rules
+echo "Editing rules.d file....\n"
+echo SUBSYSTEM==\"usb\", ENV{DEVTYPE}==\"usb_device\", ATTRS{idVendor}==\"057e\", ATTRS{idProduct}==\"0337\", MODE=\"0666\" >> /etc/udev/rules.d/gcadapter.rules
+echo "Done. Please restart or run [ sudo udevadm control --reload-rules ].""
