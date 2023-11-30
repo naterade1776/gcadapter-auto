e490685055a492d448c3e42d60b0a62cf60e3e63
Nate K <97563728+naterade1776@users.noreply.github.com>
2023-11-30 14:40:23 -0700

    Update gamecube-adapter.sh
    Removed extra double quotes and corrected the issue where script created two config files, as well as the "unexpected EOF" error.

--- gamecube-adapter.sh
+++ gamecube-adapter.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/bash
-echo "Creating rules.d file....\n"
+#!/bin/bash
+echo -e "Creating rules.d file....\n"
 touch /etc/udev/rules.d/51-gcadapter.rules
-echo "Editing rules.d file....\n"
-echo SUBSYSTEM==\"usb\", ENV{DEVTYPE}==\"usb_device\", ATTRS{idVendor}==\"057e\", ATTRS{idProduct}==\"0337\", MODE=\"0666\" >> /etc/udev/rules.d/gcadapter.rules
-echo "Done. Please restart or run [ sudo udevadm control --reload-rules ].""
+echo -e "Editing rules.d file....\n"
+echo SUBSYSTEM==\"usb\", ENV{DEVTYPE}==\"usb_device\", ATTRS{idVendor}==\"057e\", ATTRS{idProduct}==\"0337\", MODE=\"0666\" >> /etc/udev/rules.d/51-gcadapter.rules
+echo "Done. Please restart or run [ sudo udevadm control --reload-rules ]."
+exit
